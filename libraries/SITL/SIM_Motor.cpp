4d7965817bca361b74c5396776e32133452c49f0
u7091679 <u7091679@anu.edu.au>
2026-08-14 11:20:02 +1000

    BET Corolation with thrust

--- libraries/SITL/SIM_Motor.cpp
+++ libraries/SITL/SIM_Motor.cpp
@@ -157,8 +157,8 @@ void Motor::calculate_forces(const struct sitl_input &input,
         Dr_hat *= 1/sqrtf(sq(Vr.x)+sq(Vr.y));
         Vector3f Lr_hat(0,0,-1); // Unit Vector for thrust in motor frame
 
-        //thrust = rotation.mul_transpose(Lr_hat*Lr) - rotation.mul_transpose(Dr_hat*Dr); // Use line below for previous thrust implementation with Blade Element Theory Drag
-        thrust -= rotation.mul_transpose(Dr_hat*Dr);
+        thrust = rotation.mul_transpose(Lr_hat*Lr) - rotation.mul_transpose(Dr_hat*Dr); // Use line below for previous thrust implementation with Blade Element Theory Drag
+        //thrust -= rotation.mul_transpose(Dr_hat*Dr);
 
 
 
